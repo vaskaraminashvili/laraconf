aae663eb40ed62764c34258c54d68cbea90e2ccc
vasili <vasilr@giftbasketsoverseas.com>
2024-05-20 20:09:05 +0400

    PAUSE  forms vid 8 min 3:3

--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -26,40 +27,41 @@ class Speaker extends Model
     public static function getForm()
     {
         return [
-                TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                TextInput::make('email')
-                    ->email()
-                    ->required()
-                    ->maxLength(255),
-                Textarea::make('bio')
-                    ->required()
-                    ->columnSpanFull(),
-                TextInput::make('twitter_handle')
-                    ->required()
-                    ->maxLength(255),
-                CheckboxList::make('qualifications')
-                    ->options([
-                        'business-leader' => 'Business Leader',
-                        'charisma' => 'Charismatic Speaker',
-                        'first-time' => 'First Time Speaker',
-                        'hometown-hero' => 'Hometown Hero',
-                        'humanitarian' => 'Works in Humanitarian Field',
-                        'laracasts-contributor' => 'Laracasts Contributor',
-                        'twitter-influencer' => 'Large Twitter Following',
-                        'youtube-influencer' => 'Large YouTube Following',
-                        'open-source' => 'Open Source Creator / Maintainer',
-                        'unique-perspective' => 'Unique Perspective'
-                    ])
-                    ->descriptions([
-                        'business-leader' => 'Here is a nice long description',
-                        'charisma' => 'This is even more information about why you should pick this one',
-                    ])
-                    ->bulkToggleable()
-                    ->searchable()
-                    ->columnSpanFull()
-                    ->columns(3)
-            ];
+            TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+            FileUpload::make('avatar')
+                ->image()
+                ->maxSize(1024 * 1024 * 10),
+            TextInput::make('email')
+                ->email()
+                ->required()
+                ->maxLength(255),
+            Textarea::make('bio')
+                ->columnSpanFull(),
+            TextInput::make('twitter_handle')
+                ->maxLength(255),
+            CheckboxList::make('qualifications')
+                ->options([
+                    'business-leader' => 'Business Leader',
+                    'charisma' => 'Charismatic Speaker',
+                    'first-time' => 'First Time Speaker',
+                    'hometown-hero' => 'Hometown Hero',
+                    'humanitarian' => 'Works in Humanitarian Field',
+                    'laracasts-contributor' => 'Laracasts Contributor',
+                    'twitter-influencer' => 'Large Twitter Following',
+                    'youtube-influencer' => 'Large YouTube Following',
+                    'open-source' => 'Open Source Creator / Maintainer',
+                    'unique-perspective' => 'Unique Perspective'
+                ])
+                ->descriptions([
+                    'business-leader' => 'Here is a nice long description',
+                    'charisma' => 'This is even more information about why you should pick this one',
+                ])
+                ->bulkToggleable()
+                ->searchable()
+                ->columnSpanFull()
+                ->columns(3)
+        ];
     }
 }
