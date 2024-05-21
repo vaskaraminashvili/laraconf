23a03b0d60a33649a16fa9d127d5975e547d37d6
Vaso <vaska.r02@gmail.com>
2024-05-21 22:56:50 +0400

    pre vide things

--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -9,10 +9,24 @@ use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Speaker extends Model
 {
     use HasFactory;
+
+    const QUALIFICATIONS = [
+        'business-leader' => 'Business Leader',
+        'charisma' => 'Charismatic Speaker',
+        'first-time' => 'First Time Speaker',
+        'hometown-hero' => 'Hometown Hero',
+        'humanitarian' => 'Works in Humanitarian Field',
+        'laracasts-contributor' => 'Laracasts Contributor',
+        'twitter-influencer' => 'Large Twitter Following',
+        'youtube-influencer' => 'Large YouTube Following',
+        'open-source' => 'Open Source Creator / Maintainer',
+        'unique-perspective' => 'Unique Perspective'
+    ];
 
     protected $casts = [
         'id' => 'integer',
@@ -23,6 +37,12 @@ class Speaker extends Model
     {
         return $this->belongsToMany(Conference::class);
     }
+
+    public function talks(): HasMany
+    {
+        return $this->hasMany(Talk::class);
+    }
+
 
     public static function getForm()
     {
@@ -46,18 +66,7 @@ class Speaker extends Model
             TextInput::make('twitter_handle')
                 ->maxLength(255),
             CheckboxList::make('qualifications')
-                ->options([
-                    'business-leader' => 'Business Leader',
-                    'charisma' => 'Charismatic Speaker',
-                    'first-time' => 'First Time Speaker',
-                    'hometown-hero' => 'Hometown Hero',
-                    'humanitarian' => 'Works in Humanitarian Field',
-                    'laracasts-contributor' => 'Laracasts Contributor',
-                    'twitter-influencer' => 'Large Twitter Following',
-                    'youtube-influencer' => 'Large YouTube Following',
-                    'open-source' => 'Open Source Creator / Maintainer',
-                    'unique-perspective' => 'Unique Perspective'
-                ])
+                ->options(self::QUALIFICATIONS)
                 ->descriptions([
                     'business-leader' => 'Here is a nice long description',
                     'charisma' => 'This is even more information about why you should pick this one',
--- database/factories/SpeakerFactory.php
+++ database/factories/SpeakerFactory.php
@@ -20,12 +20,19 @@ class SpeakerFactory extends Factory
      */
     public function definition(): array
     {
+        $qualificationsCount = $this->faker->numberBetween(0,10);
+        $qualifications = $this->faker->randomElements(array_keys(Speaker::QUALIFICATIONS), $qualificationsCount);
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->safeEmail(),
-            'qualifications' => [],
+            'qualifications' => $qualifications,
             'bio' => $this->faker->text(),
             'twitter_handle' => $this->faker->word(),
         ];
+    }
+
+    public function withTalks(int $count = 1): self
+    {
+        return $this->has(Talk::factory()->count($count), 'talks');
     }
 }
